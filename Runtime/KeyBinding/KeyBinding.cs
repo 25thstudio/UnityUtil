3d9a3422ea2aa4625616cd0ed23102e1f906626f
Paulo Sigrist <paulo.sigrist@gmail.com>
2021-06-11 07:29:50 -0300

    KeyBinding package

---
+++ Runtime/KeyBinding/KeyBinding.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace The25thStudio.Util.KeyBinding
+{
+    [System.Serializable]
+    public class KeyBinding
+    {
+        [SerializeField] private KeyCode keyCode;
+        [SerializeField] private UnityEvent action;
+        
+        public KeyCode KeyCode => keyCode;
+        public UnityEvent Action => action;
+    }
+}
---
+++ Runtime/KeyBinding/KeyBindingBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace The25thStudio.Util.KeyBinding
+{
+    public class KeyBindingBehaviour : MonoBehaviour
+    {
+        [SerializeField] private KeyBinding[] keyBindings;
+
+
+        private void Update()
+        {
+            foreach (var binding in keyBindings)
+            {
+                if (Input.GetKeyDown(binding.KeyCode))
+                {
+                    binding.Action.Invoke();
+                }
+            }
+        }
+    }
+}
